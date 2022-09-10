98289b2d8895ba163ec5b1ec3a07f17ddb4dd177
Rahul Kumar <63224718+RahuRabh@users.noreply.github.com>
2022-09-11 00:49:50 +0530

    Add files via upload

---
+++ Student/Includes/sidebar.php
@@ -0,0 +1,37 @@
+ <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center bg-gradient-primary justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon" >
+          <img src="img/logo/attnlg.jpg">
+        </div>
+        <div class="sidebar-brand-text mx-3">UMS</div>
+      </a>
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li> 
+     
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+      Email Application
+      </div>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrapcon"
+          aria-expanded="true" aria-controls="collapseBootstrapcon">
+          <i class="fa fa-calendar-alt"></i>
+          <span>Email</span>
+        </a>
+        <div id="collapseBootstrapcon" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Manage Email</h6>
+            <a class="collapse-item" href="letter.html">Compose Email</a>
+          </div>
+        </div>
+      </li>
+
+      </li> 
+      <hr class="sidebar-divider">
+      <div class="version" id="version-ruangadmin"></div>
+    </ul>
